dfd470b27b2c542108b44d92ba0b5d875c3392d7
ArturMiguel <arturmrocha1@gmail.com>
2024-04-04 16:52:14 -0300

    Atualizados packages para compatibilidade com Flutter 3.19.5

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\Programas\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Projects\Android Studio\flutter\SalesforceMobileSDK-FlutterPlugin\example"
+export "FLUTTER_ROOT=/home/artur/Documentos/flutter/flutter-3.19.5"
+export "FLUTTER_APPLICATION_PATH=/home/artur/tecprime/projects/ssa/apps/SalesforceMobileSDK-FlutterPlugin/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
